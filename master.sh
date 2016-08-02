8305e8cfe5ca35a8a361e08529ce6f47f9e5bfba
Alex Dmitriev <dmitriev_a@rambler.ru>
2016-08-02 19:21:15 +0300

    Add scripts

---
+++ master.sh
@@ -0,0 +1,34 @@
+yum install -y tar wget git vim
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+yum install -y epel-release
+
+sudo yum groupinstall -y "Development Tools"
+sudo yum install -y apache-maven python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
+
+wget http://nodejs.org/dist/v0.10.30/node-v0.10.30-linux-x64.tar.gz
+sudo tar --strip-components 1 -xzvf node-v* -C /usr/local
+rm node-v0.10.30-linux-x64.tar.gz 
+
+rpm -Uvh http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm
+yum -y install zookeeper zookeeper-server
+sudo -u zookeeper zookeeper-server-initialize --myid=1
+sudo service zookeeper-server start
+
+rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
+yum -y install mesos-0.28.2 marathon chronos
+
+service mesos-master start
+service mesos-slave start
+service marathon start
+service chronos start
+
+
+wget https://github.com/mesosphere/mesos-dns/releases/download/v0.5.2/mesos-dns-v0.5.2-linux-amd64
+mv mesos-dns-v0.5.2-linux-amd64 /usr/local/bin/mesos-dns
+chmod +x /usr/local/bin/mesos-dns
+
+mkdir /etc/mesos-dns
+cp ./config.json /etc/mesos-dns
+
+echo "Do not forget to run mesos-dns via marathon:"
+echo "mesos-dns -config=/etc/mesos-dns/config.json"
---
+++ slave.sh
@@ -0,0 +1,7 @@
+rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
+yum -y install device-mapper-event-libs mesos-0.28.2 docker
+
+echo "docker,mesos" > /etc/mesos-slave/containerizers 
+
+echo "Do not forget to update /etc/mesos/zk with master`s IP and run command:"
+echo "service mesos-slave start"
